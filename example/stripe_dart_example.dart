07895b1b959dcb5426c586dc916ab369fa427c20
SpivEgin <spivegin@txtsme.com>
2018-12-27 10:32:34 -0500

    removed dummy text

---
+++ example/stripe_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:stripe_dart/stripe_dart.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/convert.sh
@@ -0,0 +1,3 @@
+# dart_js_facade_gen lib/src/promise.d.ts > lib/src/promise.dart
+
+dart_js_facade_gen typeScript/stripe/index.d.ts src/stripe/stripe.dart
